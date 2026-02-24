665978fef9113e2546c3cdd9e396745d3315f7ad
aniket-padyal <padyal10aniket@gmail.com>
2026-02-24 23:16:06 +0530

    update navbar name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import Showcase from "./components/Showcase";
